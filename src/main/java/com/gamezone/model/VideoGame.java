c9882b4917b59d50fdce83ab00b028e2ab0d6001
yanelis1612 <ypatriciagonzalez@unicesar.edu.co>
2026-09-14 23:44:59 -0500

    feat: add VideoGame subclass extending Product

---
+++ src/main/java/com/gamezone/model/VideoGame.java
@@ -0,0 +1,36 @@
+package com.gamezone.model;
+
+/**
+ * Represents a video game product, with attributes specific to games
+ * such as platform, genre, and age rating.
+ */
+public class VideoGame extends Product {
+    private String platform;
+    private String genre;
+    private String ageRating;
+
+    public VideoGame(String platform, String genre, String ageRating, String id, String title, double price, int stock) {
+        super(id, title, price, stock);
+        this.platform = platform;
+        this.genre = genre;
+        this.ageRating = ageRating;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getAgeRating() {
+        return ageRating;
+    }
+     
+    
+    @Override
+public String describe() {
+    return getTitle() + " is a video game for " + platform + " , genre " + genre + " , rated " + ageRating ; 
+}
+}
