301e246e7629cde95690e77fe64d502d4c1ddb3b
yanelis1612 <ypatriciagonzalez@unicesar.edu.co>
2026-09-17 10:09:47 -0500

    fix: add compatibleConsoleIds parameter to Accessory constructor and complete encapsulation

--- src/main/java/com/gamezone/model/Accessory.java
+++ src/main/java/com/gamezone/model/Accessory.java
@@ -11,18 +11,18 @@ import java.util.List;
 public abstract class Accessory extends Product {
      private List<String> compatibleConsoleIds;
  
-     /**
- * Creates a new accessory with the given common product attributes.
- * The list of compatible consoles starts empty.
- *
- * @param id the product identifier
- * @param title the product title
- * @param price the product price
- * @param stock the initial available stock
- */
+ /**
+     * Creates a new accessory with common product attributes and compatible console IDs.
+     *
+     * @param id the product identifier
+     * @param title the product title
+     * @param price the product price
+     * @param stock the initial available stock
+     * @param compatibleConsoleIds list of console IDs this accessory is compatible with
+     */
     public Accessory(String id, String title, double price, int stock) {
         super(id, title, price, stock);
-       this.compatibleConsoleIds = new ArrayList<>(); 
+       this.compatibleConsoleIds = (compatibleConsoleIds != null) ? compatibleConsoleIds : new ArrayList<>(); 
     }
     /**
  * Returns the list of console ids this accessory is compatible with.
@@ -32,12 +32,23 @@ public abstract class Accessory extends Product {
     public List<String> getCompatibleConsoleIds() {
         return compatibleConsoleIds;
     }
+    /**
+     * Sets the list of compatible console ids.
+     *
+     * @param compatibleConsoleIds list of console ids
+     */
+    public void setCompatibleConsoleIds(List<String> compatibleConsoleIds) {
+        this.compatibleConsoleIds = compatibleConsoleIds;
+    }
  /**
  * Registers a console as compatible with this accessory.
  *
  * @param consoleId the id of the console to add
  */   
     public void addCompatibleConsole(String consoleId) {
+        if (this.compatibleConsoleIds == null) {
+            this.compatibleConsoleIds = new ArrayList<>();
+        }
         this.compatibleConsoleIds.add(consoleId);
     }
   /**
@@ -46,9 +57,8 @@ public abstract class Accessory extends Product {
  * @param consoleId the id of the console to check
  * @return true if the console id is in the compatible list
  */  
-    public boolean isCompatibleWith(String consoleId) {
-        return this.compatibleConsoleIds.contains(consoleId);
+  public boolean isCompatibleWith(String consoleId) {
+        return this.compatibleConsoleIds != null && this.compatibleConsoleIds.contains(consoleId);
     }
+} 
     
-    
-}
