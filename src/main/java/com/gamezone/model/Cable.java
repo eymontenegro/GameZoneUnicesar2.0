05376b4384984646f3f254dd6a3349d6e2613fba
yanelis1612 <ypatriciagonzalez@unicesar.edu.co>
2026-09-17 12:08:54 -0500

    refactor: update Cable constructor to match updated Accessory superclass

--- src/main/java/com/gamezone/model/Cable.java
+++ src/main/java/com/gamezone/model/Cable.java
@@ -1,8 +1,6 @@
 
 package com.gamezone.model;
 
-import java.util.List;
-
 /**
  * Concrete class representing a connection cable accessory.
  * Extends the abstract Accessory class.
@@ -10,7 +8,7 @@ import java.util.List;
 public class Cable extends Accessory {
     private double length;
     private String connectorType;
-    
+
     /**
      * Constructs a new Cable accessory with the specified details.
      *
@@ -18,34 +16,58 @@ public class Cable extends Accessory {
      * @param title the product title or name
      * @param price the price of the cable
      * @param stock the initial available stock quantity
-     * @param compatibleConsoleIds list of console IDs compatible with this cable
      * @param length the length of the cable in meters
      * @param connectorType the type of connector (e.g., "HDMI", "USB-C")
      */
-    public Cable(double length, String connectorType, String id, String title, double price, int stock, List<String> compatibleConsoleIds) {
-        super(id, title, price, stock, compatibleConsoleIds);
+    public Cable(String id, String title, double price, int stock, double length, String connectorType) {
+        super(id, title, price, stock);
         this.length = length;
         this.connectorType = connectorType;
     }
+
+    /**
+     * Gets the length of the cable in meters.
+     *
+     * @return the cable length
+     */
     public double getLength() {
         return length;
     }
 
+    /**
+     * Sets the length of the cable in meters.
+     *
+     * @param length the cable length to set
+     */
     public void setLength(double length) {
         this.length = length;
     }
 
+    /**
+     * Gets the connector type of the cable.
+     *
+     * @return the connector type
+     */
     public String getConnectorType() {
         return connectorType;
     }
 
+    /**
+     * Sets the connector type of the cable.
+     *
+     * @param connectorType the connector type to set
+     */
     public void setConnectorType(String connectorType) {
         this.connectorType = connectorType;
     }
 
+    /**
+     * Provides a text description of the cable.
+     *
+     * @return a formatted description string
+     */
     @Override
     public String describe() {
         return getTitle() + " es un cable de conector " + connectorType + " con longitud de " + length + "m, precio: $" + getPrice() + ", stock: " + getStock();
     }
-
 }
